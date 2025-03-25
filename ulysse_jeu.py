9ce46a8ac2cb4ad7d3f2afe62f4d696858083b53
ulysse <ulyssedallais@gmail.com>
2025-03-25 14:15:48 +0100

    Update ulysse_jeu.py

--- ulysse_jeu.py
+++ ulysse_jeu.py
@@ -15,4 +15,13 @@ BLEU = (0, 0, 255)
 fenetre = pygame.display.set_mode((LARGEUR, HAUTEUR))
 pygame.display.set_caption("Démineur de la Tour")
 
+def generer_mines(taille):
+    mines = []
+    nb_mines = (taille ** 2) // 5    # on a taille = 8, donc 8^2=64 et 64//5 environ = à 12, ya 12 mines en tout a chaque fois
+    while len(mines) < nb_mines:
+        x = random.randint(0, taille - 1)
+        y = random.randint(0, taille - 1)
+        if (x, y) not in mines:
+            mines.append((x, y))
+    return mines
 
